831713897c304fa6a6ddcd126545dbeea6c30293
Ali <molaali.ali@gmail.com>
2022-12-09 15:57:37 +0330

    Initial commit

---
+++ src/main/java/com/ali/springthymeleafquotes/SpringThymeleafQuotesApplication.java
@@ -0,0 +1,13 @@
+package com.ali.springthymeleafquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafQuotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringThymeleafQuotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ali/springthymeleafquotes/SpringThymeleafQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ali.springthymeleafquotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafQuotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
